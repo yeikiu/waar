1807a19cf7a70ba228afe6d818c8401ec23cdc75
JQ <jqocon@gmail.com>
2020-01-08 01:42:58 +0000

    chore: lint

--- src/handlers/browser_handler.ts
+++ src/handlers/browser_handler.ts
@@ -1,64 +1,65 @@
 // Output setup
-import debugHelper from "./../util/debug_helper";
+// eslint-disable-next-line no-unused-vars
+import { Browser, Page } from 'puppeteer';
+import debugHelper from '../util/debug_helper';
+
+// eslint-disable-next-line no-unused-vars
 const { debug, logError, print } = debugHelper(__filename);
 
 const path = require('path');
-const puppeteer = require("puppeteer");
-import { Browser, Page } from "puppeteer";
+const puppeteer = require('puppeteer');
 
 const doQRlogin = async (page: Page): Promise<Page> => {
-    print(`Login required, please wait while QR code is generated`);
-    await page.waitForSelector('img[alt="Scan me!"]', { timeout: 0 });
-    print(`Please scan the QR code with your phone's WhatsApp scanner`);
+  print('Login required, please wait while QR code is generated');
+  await page.waitForSelector('img[alt="Scan me!"]', { timeout: 0 });
+  print('Please scan the QR code with your phone\'s WhatsApp scanner');
 
-    await page.waitForSelector('#pane-side', { timeout: 0 });
-    print('🙌  Logged IN! 🙌');
-    return page;
-}
+  await page.waitForSelector('#pane-side', { timeout: 0 });
+  print('🙌  Logged IN! 🙌');
+  return page;
+};
 
 export default {
 
-    launchBrowser(dataDirPath: string): Browser {
-        return puppeteer.launch({
-            // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions
-            headless: false,
-            args: [
-                `--user-data-dir=${path.resolve(dataDirPath)}`,
-                "--disable-dev-shm-usage"
-            ], 
-            //executablePath: CHROME_PATH
-        });
-    },
+  launchBrowser(dataDirPath: string): Browser {
+    return puppeteer.launch({
+      // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions
+      headless: false,
+      args: [
+        `--user-data-dir=${path.resolve(dataDirPath)}`,
+        '--disable-dev-shm-usage',
+      ],
+      // executablePath: CHROME_PATH
+    });
+  },
 
-    async loadWhatsappWeb(browser: Browser): Promise<Page> {
-        const page = (await browser.pages())[0];
-        page.setViewport({ width: 1024, height: 768 });
-        await page.goto('https://web.whatsapp.com/', {
-            waitUntil: 'networkidle2',
-            timeout: 0
-        });
+  async loadWhatsappWeb(browser: Browser): Promise<Page> {
+    const page = (await browser.pages())[0];
+    page.setViewport({ width: 1024, height: 768 });
+    await page.goto('https://web.whatsapp.com/', {
+      waitUntil: 'networkidle2',
+      timeout: 0,
+    });
 
-        // Let the UI load
-        await Promise.race([
-            page.waitForSelector('#pane-side', { timeout: 0 }),
-            page.waitForSelector('.landing-title', { timeout: 0 })
-        ]);
-        print(`Whatsapp-Web Loaded!`);
+    // Let the UI load
+    await Promise.race([
+      page.waitForSelector('#pane-side', { timeout: 0 }),
+      page.waitForSelector('.landing-title', { timeout: 0 }),
+    ]);
+    print('Whatsapp-Web Loaded!');
 
-        // Check if login is needed
-        let title = null;
-        try {
-            title = await page.$eval('.landing-title', (t) => {
-                if (!t) return null;
-                return t.textContent;
-            });
-    
-        } catch (e) { print(`Already logged in, loading chats...`); }
-        
-        if (title === null)
-            return page;
+    // Check if login is needed
+    let title = null;
+    try {
+      title = await page.$eval('.landing-title', (t) => {
+        if (!t) return null;
+        return t.textContent;
+      });
+    } catch (e) { print('Already logged in, loading chats...'); }
 
-        // QR login needed
-        return doQRlogin(page);
-    }
-}
+    if (title === null) { return page; }
+
+    // QR login needed
+    return doQRlogin(page);
+  },
+};
--- src/util/debug_helper.ts
+++ src/util/debug_helper.ts
@@ -1,22 +1,28 @@
+/* eslint-disable global-require */
+const path = require('path');
+const debugLib = require('debug');
+
 const getPackageName = (): string => {
-    // Load Project name from package.json file
-    try {
-        const pkg = require(require("path").resolve("package"));
-        if (!pkg.name) throw new Error();
-        return pkg.name;
-    } catch ({ code, message }) {
-        throw new Error(
-            "Can´t load name from ./package.json file.\nDoes it exist under root directory?"
-        );
-    }
+  // Load Project name from package.json file
+  try {
+    const packagePath = path.resolve('package');
+    // eslint-disable-next-line import/no-dynamic-require
+    const pkg = require(packagePath);
+    if (!pkg.name) throw new Error();
+    return pkg.name;
+  } catch ({ code, message }) {
+    throw new Error(
+      'Can´t load name from ./package.json file.\nDoes it exist under root directory?',
+    );
+  }
 };
 
 export default (fileName: string, label: string = getPackageName()) => {
-    const filePath = require("path").parse(fileName).name;
+  const filePath = path.parse(fileName).name;
 
-    const debug = require("debug")(`${label}:${filePath}`);
-    const logError = require("debug")(`${label}:${filePath}:error*`);
-    const print = require("debug")(`${label}:${filePath}:*`);
+  const debug = debugLib(`${label}:${filePath}`);
+  const logError = debugLib(`${label}:${filePath}:error*`);
+  const print = debugLib(`${label}:${filePath}:*`);
 
-    return { debug, logError, print };
+  return { debug, logError, print };
 };
