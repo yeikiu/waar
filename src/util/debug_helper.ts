b6a663b3c6ec2f9e7bc8ca76fa2caabb0e567bf4
JQ <jqocon@gmail.com>
2020-09-13 23:44:11 +0100

    v1.1.22

--- src/util/debug_helper.ts
+++ src/util/debug_helper.ts
@@ -1,15 +1,15 @@
-import { parse } from 'path';
-import debugLib from 'debug';
+import { parse } from 'path'
+import debugLib, { Debugger } from 'debug'
 
 /* RECOMMENED TO MATCH PROJECT'S NAME */
 const DEBUG_PREFIX = 'waar'
 
-export default (fileName: string, label = DEBUG_PREFIX): { debug: Function; logError: Function; print: Function } => {
-  const filePath = parse(fileName).name;
+export default (fileName: string, label = DEBUG_PREFIX): { debug: Debugger; logError: Debugger; print: Debugger; } => {
+  const filePath = parse(fileName).name
 
-  const debug = debugLib(`${label}:${filePath}`);
-  const logError = debugLib(`${label}:${filePath}:error*`);
-  const print = debugLib(`${label}:${filePath}:*`);
+  const debug = debugLib(`${label}:${filePath}`)
+  const logError = debugLib(`${label}:${filePath}:error*`)
+  const print = debugLib(`${label}:${filePath}:*`)
 
-  return { debug, logError, print };
+  return { debug, logError, print }
 };
