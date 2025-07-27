a1d2157219303ce9cdce37b502c9834513d89286
JQ <jqocon@gmail.com>
2025-07-27 20:43:58 +0100

    refactor: dont use .$eval(...)

--- src/playwright_handlers/do_qr_login.ts
+++ src/playwright_handlers/do_qr_login.ts
@@ -2,14 +2,14 @@ import * as qrcode from 'qrcode-terminal';
 import { debugHelper } from '../util/debug_helper';
 import { Page } from 'playwright';
 
-const { debug, print } = debugHelper(__filename);
+const { print } = debugHelper(__filename);
 
 export const doQRlogin = async (page: Page): Promise<Page> => {
   print('Login required! Please wait while QR code is generated... ⏳');
   await page.waitForSelector('div[data-ref]', { timeout: 0 });
 
-  const dataRef = await page.$eval('div[data-ref]', div => div.getAttribute('data-ref'));
-  debug({ dataRef });
+  const dataRefDiv = page.locator('div[data-ref]');
+  const dataRef = await dataRefDiv.getAttribute('data-ref');
   qrcode.generate(dataRef, { small: true });
 
   print('Please scan the QR code above with your phone\'s WhatsApp scanner ☝️');
